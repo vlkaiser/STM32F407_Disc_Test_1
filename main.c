759b4f787e36da3a9a05e93e6616d2893c718930
Vicki <vlkaiser9@gmail.com>
2019-09-27 16:41:06 -0400

    Initial Commit - Core program, no functionality

---
+++ main.c
@@ -0,0 +1,29 @@
+/***************************************************************************************************************************
+* This file is subject to the terms and conditions defined in file 'LICENSE.txt', 
+* which is part of this source code package. 
+*
+* Project									: MCU Init and Blinky LED
+* Program Name						: Blinky_STM32_Discovery.uvprojx
+* Author									: vkaiser
+* Date Created						: Sept 27, 2019
+* 
+* Purpose									: Initialize eval board and operate GPIO
+* MCU											: STM32F407VGT6
+* Language								: C
+* Hardware Modifications	: N/A
+*
+* Repo / Revision History	: https://github.com/vlkaiser/STM32F407_Disc_Test_1
+*
+* - Special Setup -
+* Keil Pack Installer 		: Device Specific DFP
+* 
+* Revision History				:
+* 	Date				Author			Notes
+* 09272019			vkaiser			- Initial commit
+*
+***************************************************************************************************************************/
+
+int main(void)
+{
+	return 0;
+}
